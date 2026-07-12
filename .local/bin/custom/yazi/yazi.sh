2feeba0e3f19036deb487e5f988d2a6f6638426a
Eugénio Mello <none@none.com>
2026-07-12 20:54:59 +0100

    Added Yazi launch script
    Added yazi launch script with auto cd on exit

---
+++ .local/bin/custom/yazi/yazi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+function yy() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  command yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd <"$tmp"
+  [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+  rm -f -- "$tmp"
+}
+
+yy
