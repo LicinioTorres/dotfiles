bf4b7e18f593980c843805a5c47aa2fd42cf7dd0
Eugénio Mello <none@none.com>
2026-07-13 11:24:30 +0100

    Updated Hyprlock
    Added Wallpaper
    Autostart Hyprlock on hyprland Startup

--- .config/hypr/hyprland/autostart.lua
+++ .config/hypr/hyprland/autostart.lua
@@ -1,8 +1,9 @@
 hl.on("hyprland.start", function () 
   hl.exec_cmd("numlockx on") --Start with numlock on
+  hl.exec_cmd("hyprlock") -- Start Hyprlock
   hl.exec_cmd("systemctl") --user start hyprpolkitagent
   hl.exec_cmd("wl-paste") --watch cliphist store # execute listener for cliphist
   hl.exec_cmd("waybar & hyprpaper") -- Execute waybar, hyprpaper
-	hl.exec_cmd("systemctl --user start hyprpolkitagent")
-  hl.exec_cmd("systemctl --user start hyprland-session.target")
+	hl.exec_cmd("systemctl --user start hyprpolkitagent") -- Start hyprland utils
+  hl.exec_cmd("systemctl --user start hyprland-session.target") -- Start hyprland-session.target (uwsm was not starting it auto)
 end)
