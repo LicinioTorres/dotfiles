ea22619c92f3d95d9f56d0a78b9c8f02ecdfa2f8
Eugénio Mello <none@none.com>
2026-07-13 18:43:22 +0100

    Fixed Bug on hyprland monitors module
    Incorrect relative positioning of monitor 2
    Missing persistant flag on monitor specific workspaces causing monitor 2
    to not have created workspaces

--- .config/hypr/hyprland/monitors.lua
+++ .config/hypr/hyprland/monitors.lua
@@ -6,49 +6,58 @@ hl.monitor({
     output = "HDMI-A-2",
     mode = "1920x1080@60",
     position = "1920x0",
-    scale = 1.33 ,
+    scale = 1.5 ,
 })
 hl.monitor({
-    output = "DP-0",
+    output = "eDP-1",
     mode = "1920x1080@60",
-    position = "1920x0",
-    scale = 1.33 ,
+    position = "0x0",
+    scale = 1.2 ,
 })
 
 -- Workspaces
 hl.workspace_rule({
   workspace = "1",
   monitor = "HDMI-A-2",
+  persistent = true
 })
 hl.workspace_rule({
   workspace = "2",
-  monitor = "DP-0",
+  monitor = "eDP-1",
+  persistent = true
 })
 hl.workspace_rule({
   workspace = "3",
   monitor = "HDMI-A-2",
+  persistent = true
 })
 hl.workspace_rule({
   workspace = "4",
-  monitor = "DP-0",
+  monitor = "eDP-1",
+  persistent = true
 })
 hl.workspace_rule({
   workspace = "5",
   monitor = "HDMI-A-2",
+  persistent = true
 })
 hl.workspace_rule({
   workspace = "6",
-  monitor = "DP-0",
+  monitor = "eDP-1",
+  persistent = true
 })
 hl.workspace_rule({
   workspace = "7",
   monitor = "HDMI-A-2",
+  persistent = true
 })
 hl.workspace_rule({
   workspace = "8",
-  monitor = "DP-0",
+  monitor = "eDP-1",
+  persistent = true
 })
 hl.workspace_rule({
   workspace = "9",
   monitor = "HDMI-A-2",
+  persistent = true
 })
